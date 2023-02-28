10b75d8d7ff865968e61322a4995164f5bf4b9af
kyooowe <brinasjohnmark@gmail.com>
2023-02-28 23:42:30 +0800

    remove whitespaces

--- src/helpers/mailer.helper.ts
+++ src/helpers/mailer.helper.ts
@@ -1,31 +1,29 @@
 //#region Import
-import nodemailer from 'nodemailer'
-import { IMailer } from '../interface/mailer.interface'
+import nodemailer from 'nodemailer';
+import { IMailer } from '../interface/mailer.interface';
 //#endregion
 
 const Mailer = async ({ from, to, subject, text, html }: IMailer) => {
+	// Reusable transport object using default SMTP
+	const transporter = nodemailer.createTransport({
+		host: 'smtp.gmail.com',
+		port: 465,
+		secure: true,
+		auth: {
+			user: `${process.env.MAILER_USER}`,
+			pass: `${process.env.MAILER_PASSWORD}`
+		}
+	});
 
-    // Reusable transport object using default SMTP
-    const transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 465,
-        secure: true,
-        auth: {
-            user: `${process.env.MAILER_USER}`,
-            pass: `${process.env.MAILER_PASSWORD}`
-        }
-    });
-
-    
-    await transporter.sendMail({
-        from: `${from}`,
-        to: `${to}`,
-        subject: `${subject}`,
-        text: `${text}`,
-        html: `${html}`
-    });
-}
+	await transporter.sendMail({
+		from: `${from}`,
+		to: `${to}`,
+		subject: `${subject}`,
+		text: `${text}`,
+		html: `${html}`
+	});
+};
 
 export const MailerHelper = {
-    Mailer
-}
+	Mailer
+};
